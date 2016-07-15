da0c6441cff413cd32dc31b1fe978b071f590ebf
Sergio Saborío Taylor <ssergio92@gmail.com>
2016-07-14 21:59:57 -0600

    new: dev: Add init fragment with proper toolbar.

--- GoingOn/app/src/main/java/de/andreasschrade/goingon/ui/signing/SigningActivity.java
+++ GoingOn/app/src/main/java/de/andreasschrade/goingon/ui/signing/SigningActivity.java
@@ -10,16 +10,15 @@ public class SigningActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        setContentView(R.layout.activity_signing);
 
         // Show the Up button in the action bar.
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        // TODO Call the signup fragment
-        //ArticleDetailFragment fragment =  ArticleDetailFragment.newInstance(getIntent().getStringExtra(ArticleDetailFragment.ARG_ITEM_ID));
-        //getFragmentManager().beginTransaction().replace(R.id.article_detail_container, fragment).commit();
+        InitFragment fragment =  InitFragment.newInstance(getIntent().getStringExtra(InitFragment.ARG_ITEM_ID));
+        getFragmentManager().beginTransaction().replace(R.id.signing_container, fragment).commit();
     }
 
     @Override
